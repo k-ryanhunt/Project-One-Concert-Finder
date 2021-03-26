41baeab404090b6b0a95e33b308bff6cd303bd16
Ducky <staubbm@gmail.com>
2021-03-25 21:35:23 -0600

    one of the two apis working

--- assests/logic.js
+++ assests/logic.js
@@ -1,49 +1,55 @@
-var text = $('#buttonText').val();
-function deezer (){
-fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "8d44b498cbmsh2179a60ff84b2a4p19a6fajsn4830609bdf3b",
-		"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
+var text = ''
+var lat = '';
+var long = '';
+var buttonsearch = $("#buttonSearch")
+var bandName = '';
+
+
+
+
+
+
+function deezer() {
+    fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=creed", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": "8d44b498cbmsh2179a60ff84b2a4p19a6fajsn4830609bdf3b",
+                "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
+            }
+        })
+        .then(response => {
+            console.log(response);
+        })
+        .catch(err => {
+            console.error(err);
+        });
 
 }
 
-function ticketMaster(){
-fetch("https://app.ticketmaster.com/discovery/v2/venue/"+text+".json?apikey=FHlXGGy5pkbqcfKrq9TAErHgeV0EHiMb", {
-	"method": "GET"
-	}
-)
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
-}
 
 function button() {
-	ticketMaster()
-	deezer()
+    text = $('.buttonText').val();
+    ticketmaster()
+    deezer()
+    console.log(text)
 }
-// $.ajax({
-//     type:"GET",
-//     url:"https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=FHlXGGy5pkbqcfKrq9TAErHgeV0EHiMb",
-//     async:true,
-//     dataType: "json",
-//     success: function(json) {
-//                 console.log(json);
-//                 // Parse the response.
-//                 // Do other things.
-//              },
-//     error: function(xhr, status, err) {
-//                 // This time, we do not end up here!
-//              }
-//   });
+
+
+function ticketmaster() {
+    $.ajax({
+        type: "GET",
+        url: "https://app.ticketmaster.com/discovery/v2/events.json?apikey=FHlXGGy5pkbqcfKrq9TAErHgeV0EHiMb&city=" + text + "&classificationName=music&size=5",
+        async: true,
+        dataType: "json",
+        success: function(json) {
+            console.log(json);
+            // Parse the response.
+            // Do other things.
+        },
+        error: function(xhr, status, err) {
+            // This time, we do not end up here!
+        }
+    });
+}
+
+buttonsearch.on('click', button);
