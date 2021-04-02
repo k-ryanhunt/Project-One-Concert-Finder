15482029fdfa4d520bfa00431b48512c9ff4c464
K Ryan <hunt.kelsearyan@gmail.com>
2021-04-01 23:23:55 -0600

    Local storage should be working

--- assets/logic.js
+++ assets/logic.js
@@ -1,15 +1,18 @@
-var text = ''
 var buttonsearch = $("#buttonSearch")
 var band = []
 var count = 0
 var history = $(".History")
-    // var bandName = '';
+
 function button() {
     text = $('.buttonText').val();
     ticketmaster()
     band = []
     localStorage.setItem(count, text)
-    count = count + 1
+    for (var i = 0; i < localStorage.length; i++) {
+        var city = localStorage.getItem(i);
+        var cityName = $(".History").addClass("listGroupItem");
+        cityName.append('<li>' + city + '</li>');
+    }
 }
 
 function ticketmaster() {
@@ -19,7 +22,7 @@ function ticketmaster() {
         url: "https://app.ticketmaster.com/discovery/v2/events.json?apikey=FHlXGGy5pkbqcfKrq9TAErHgeV0EHiMb&city=" + text + "&classificationName=music&size=5",
         async: true,
         dataType: "json",
-        success: function(json) {
+        success: function (json) {
             // console.log(json)
             if (!json._embedded) {
                 $(".searchResults").append("<div class='eventInfo card-panel small center-align'>" + "There are no events in this city. <br> Please enter another city." + "</div>")
@@ -30,17 +33,15 @@ function ticketmaster() {
                 } else {
                     for (var i = 0; i < eventName.length; i++) {
                         var artistName = json._embedded.events[i]._embedded.attractions[0].name
-                            // console.log(artistName)
                         band.push(artistName)
                         var bandName = eventName[i].name
                         var venueName = eventName[i]._embedded.venues[0].name
-                            // second api gives us the variable "aristInfo"
                         if (!eventName[i].priceRanges) {
                             $(".searchResults").append("<div class='eventInfo card-panel small center-align'>" +
                                 "<li class='headliner'>" + bandName + "</li>" +
                                 "<li class='venue'>" + "Will be playing at the" + "</li>" +
                                 "<li class='venueName'>" + venueName + "</li>" +
-                                "<li class='price'>" + "There are no avaliable ticket prices</li>" +
+                                "<li class='price'>" + "There are no avaliable ticket prices" + "</li>" +
                                 "<li class = 'artistInfoResults" + [i] + "'> <li>" +
                                 "<a class='btn-floating btn-large pulse red accent-2' href=" + searchUrl + " target = _blank>" + "Buy" + "</a>" + "</div>")
                         } else {
@@ -59,7 +60,7 @@ function ticketmaster() {
                     let LastFM0 = 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + band[0] + '&api_key=78661b1408a61c6f77a83efc09f78da4&format=json'
                     fetch(LastFM0)
                         .then(
-                            function(response) {
+                            function (response) {
                                 return response.json();
                             })
                         .then(data => {
@@ -70,7 +71,7 @@ function ticketmaster() {
                     let LastFM1 = 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + band[1] + '&api_key=78661b1408a61c6f77a83efc09f78da4&format=json'
                     fetch(LastFM1)
                         .then(
-                            function(response) {
+                            function (response) {
                                 return response.json();
                             })
                         .then(data => {
@@ -80,7 +81,7 @@ function ticketmaster() {
                     let LastFM2 = 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + band[2] + '&api_key=78661b1408a61c6f77a83efc09f78da4&format=json'
                     fetch(LastFM2)
                         .then(
-                            function(response) {
+                            function (response) {
                                 return response.json();
                             })
                         .then(data => {
@@ -90,7 +91,7 @@ function ticketmaster() {
                     let LastFM3 = 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + band[3] + '&api_key=78661b1408a61c6f77a83efc09f78da4&format=json'
                     fetch(LastFM3)
                         .then(
-                            function(response) {
+                            function (response) {
                                 return response.json();
                             })
                         .then(data => {
@@ -100,7 +101,7 @@ function ticketmaster() {
                     let LastFM4 = 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + band[4] + '&api_key=78661b1408a61c6f77a83efc09f78da4&format=json'
                     fetch(LastFM4)
                         .then(
-                            function(response) {
+                            function (response) {
                                 return response.json();
                             })
                         .then(data => {
@@ -110,17 +111,9 @@ function ticketmaster() {
                 };
             }
         },
-        error: function(xhr, status, err) {
-            // This time, we do not end up here!
+        error: function (xhr, status, err) {
         }
     });
 }
+
 buttonsearch.on('click', button);
-
-
-
-for (var i = 0; i < localStorage.length; i++) {
-    var city = localStorage.getItem(i);
-    var cityName = $(".History").addClass("listGroupItem");
-    cityName.append('<li>' + city + '</li>');
-}
