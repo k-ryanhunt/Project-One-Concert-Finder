8c45ecfe1dc2ed23a2e7c8567446ab2d097be556
Mark Raudenbush <markraud@hotmail.com>
2021-04-01 20:42:27 -0600

    marks commit

--- assets/logic.js
+++ assets/logic.js
@@ -5,10 +5,8 @@ var band = []
 function button() {
     text = $('.buttonText').val();
     ticketmaster()
-    // console.log(text)
-        // console.log(artistName)
+    band = []
 }
-
 function ticketmaster() {
     $(".searchResults").empty();
     $.ajax({
@@ -17,7 +15,7 @@ function ticketmaster() {
         async: true,
         dataType: "json",
         success: function(json) {
-            console.log(json)
+            // console.log(json)
             if (!json._embedded) {
                 $(".searchResults").append("<div class='eventInfo card-panel small center-align'>" + "There are no events in this city. <br> Please enter another city." + "</div>")
             } else {
@@ -28,72 +26,93 @@ function ticketmaster() {
                     for (var i = 0; i < eventName.length; i++) {
                         var artistName = json._embedded.events[i]._embedded.attractions[0].name
                         // console.log(artistName)
-                        band = [artistName]
-                        // console.log(band)
+                        band.push(artistName)
                         var bandName = eventName[i].name
                         var venueName = eventName[i]._embedded.venues[0].name
                             // second api gives us the variable "aristInfo"
-                        var LastFM = 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + band + '&api_key=78661b1408a61c6f77a83efc09f78da4&format=json'
-
                         if (!eventName[i].priceRanges) {
-
-                            fetch(LastFM)
-                            .then(
-                                function(response) {
-                                    return response.json();
-                                })
-                            .then(data => {
-                                    var artistInfo = data.artist[i].bio.summary
-                                    $(".searchResults").append("<div class='eventInfo card-panel small center-align'>" +
-                                    "<li class='headliner'>" + bandName + "</li>" +
-                                    "<li class='venue'>" + "will be playing at the " + "</li>" +
-                                    "<li class='venueName'>" + venueName + "</li>" +
-                                    "<li class='price'>" + "there is no price ranges availible" + "</li>" +
-                                    "<li>" + artistInfo + "<li>" +
-                                    "<a class='btn-floating btn-large pulse red accent-2' href=" + searchUrl + " target = _blank>" + "Buy" + "</a>" + "</div>")
-                                    
-                                }
-
-                            )
-
+                            $(".searchResults").append("<div class='eventInfo card-panel small center-align'>" +
+                                "<li class='headliner'>" + bandName + "</li>" +
+                                "<li class='venue'>" + "will be playing at the" + "</li>" +
+                                "<li class='venueName'>" + venueName + "</li>" +
+                                "<li class='price'>" + "there is no ticket prices available </li>" +
+                                "<li class = 'artistInfoResults" + [i] + "'> <li>" +
+                                "<a class='btn-floating btn-large pulse red accent-2' href=" + searchUrl + " target = _blank>" + "Buy" + "</a>" + "</div>")
                         } else {
                             var minPrice = eventName[i].priceRanges[0].min
                             var maxPrice = eventName[i].priceRanges[0].max
                             var searchUrl = eventName[i].url
-                            
-                            
                             $(".searchResults").append("<div class='eventInfo card-panel small center-align'>" +
                                 "<li class='headliner'>" + bandName + "</li>" +
                                 "<li class='venue'>" + "will be playing at the" + "</li>" +
                                 "<li class='venueName'>" + venueName + "</li>" +
                                 "<li class='price'>" + "Tickets range from: " + "$" + minPrice + " to " + "$" + maxPrice + "</li>" +
-                                "<li class = 'artistInfoResults'> <li>" +
-                                "<a class='btn-floating btn-large pulse red accent-2' href=" + searchUrl + " target = _blank>" + "Buy" + "</a>" + "</div>")
-                                fetch(LastFM)
+                                "<li class = 'artistInfoResults" + [i] + "'> <li>" +
+                                "<a class='btn-floating btn-large pulse red accent-2' href=" + searchUrl + " target = _blank>" + "Buy" + "</a>" + "</div>") 
+                        }
+                    }
+                    let LastFM0 = 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + band[0]+ '&api_key=78661b1408a61c6f77a83efc09f78da4&format=json'
+                    fetch(LastFM0)
+                        .then(
+                            function (response) {
+                                return response.json();
+                            })
+                        .then(data => {
+                            $(".artistInfoResults0").empty();
+                            var artistInfo = data.artist.bio.summary
+                            $(".artistInfoResults0").text(artistInfo)
+                        }
+                        )
+                        let LastFM1 = 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + band[1]+ '&api_key=78661b1408a61c6f77a83efc09f78da4&format=json'
+                        fetch(LastFM1)
+                            .then(
+                                function (response) {
+                                    return response.json();
+                                })
+                            .then(data => {
+                                var artistInfo = data.artist.bio.summary
+                                $(".artistInfoResults1").text(artistInfo)
+                            }
+                            )
+                            let LastFM2 = 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + band[2]+ '&api_key=78661b1408a61c6f77a83efc09f78da4&format=json'
+                            fetch(LastFM2)
                                 .then(
-                                    function(response) {
+                                    function (response) {
                                         return response.json();
                                     })
                                 .then(data => {
-                                        //this is appending artist info to the above class made on line 68
+                                    var artistInfo = data.artist.bio.summary
+                                    $(".artistInfoResults2").append(artistInfo)
+                                }
+                                )
+                                let LastFM3 = 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + band[3]+ '&api_key=78661b1408a61c6f77a83efc09f78da4&format=json'
+                                fetch(LastFM3)
+                                    .then(
+                                        function (response) {
+                                            return response.json();
+                                        })
+                                    .then(data => {
                                         var artistInfo = data.artist.bio.summary
-                                        console.log(data)
-                                        $(".artistInfoResults").append(artistInfo)
-                                        
+                                        $(".artistInfoResults3").append(artistInfo)
                                     }
-    
-                                )     
-                        }
-                    }
-    
+                                    )
+                                    let LastFM4 = 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + band[4]+ '&api_key=78661b1408a61c6f77a83efc09f78da4&format=json'
+                                    fetch(LastFM4)
+                                        .then(
+                                            function (response) {
+                                                return response.json();
+                                            })
+                                        .then(data => {
+                                            var artistInfo = data.artist.bio.summary
+                                            $(".artistInfoResults4").append(artistInfo)
+                                        }
+                                        )
                 };
             }
         },
-
         error: function(xhr, status, err) {
             // This time, we do not end up here!
         }
     });
 }
-
 buttonsearch.on('click', button);
